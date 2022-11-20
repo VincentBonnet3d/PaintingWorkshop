55b2d78290948c13d19ec408778fca2ef3b9f727
VincentBonnet3d <pro.vincentbonnet@gmail.com>
2022-11-20 20:15:05 +0100

    Unity Project Save - almost finished

--- PaintingWorkshop/Assets/Gameplay/Teleportation.cs
+++ PaintingWorkshop/Assets/Gameplay/Teleportation.cs
@@ -11,14 +11,14 @@ public class Teleportation : MonoBehaviour
         
         if(Input.GetKey(KeyCode.T))
         {
-            Debug.Log("Load scene");
-            SceneManager.LoadScene("Scene_PaintingWorkshop_salleVG");
+            Initiate.Fade("Scene_PaintingWorkshop_salleVG", Color.black, 1);
         }
 
         if (Input.GetKey(KeyCode.Y))
         {
-            Debug.Log("Load scene");
-            SceneManager.LoadScene("Scene_PaintingWorkshop_Atelier");
+            Initiate.Fade("Scene_PaintingWorkshop_Atelier", Color.black, 1);
         }
+
     }
+
 }
